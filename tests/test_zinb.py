152b1e48bee241b21ccb620c35cfe791198d2383
HH197 <hamid__hamidi@outlook.com>
2023-06-27 17:14:31 -0600

    Added conftest.py, updated pyproject.toml and test_zinb.py
    The conftest.py is created for the first time.
    
    The pyproject.toml is updated with necessary configuration for testing
    The test_zinb.py is updated to use the synthetic data set for tests.
    
    This is just the for starting the testing pipeline.

--- tests/test_zinb.py
+++ tests/test_zinb.py
@@ -2,33 +2,41 @@
 An initial test for testing the packages in production.
 """
 
-import grad.ZINB_grad as ZINB_grad
 import torch
-import grad.data_prep as data_prep
-import grad.helper as helper
 from torch.utils.data import DataLoader
 
-torch.manual_seed(197)
-device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-cortex = data_prep.CORTEX(file_dir="/home/hh197/Data/Thesis/Data/new.txt")
-y, labels = next(iter(DataLoader(cortex, batch_size=cortex.n_cells, shuffle=True)))
+import grad.ZINB_grad as ZINB_grad
 
 
-model = ZINB_grad.ZINB_Grad(Y=y, K=10, device=device)
-y = y.to(device)
-model.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.08)
-losses, neg_log_liks = ZINB_grad.train_ZINB(y, optimizer, model, epochs=150)
+def test_learning(synth_data, device):
 
-helper.plot_line(list(range(len(losses))), losses)
-
-w = model.W.cpu().detach().numpy()
-
-sil_coeff = helper.kmeans(w)
-
-helper.plot_line(list(range(len(sil_coeff))), sil_coeff)
+    torch.manual_seed(197)
 
 
-labels = labels.numpy()
+    y = next(iter(DataLoader(
+        synth_data,
+        batch_size=synth_data.n_samples,
+        shuffle=True
+    )))
 
-helper.measure_q(w, labels, n_clusters=7)
+    model = ZINB_grad.ZINB_Grad(Y=y, K=10, device=device)
+    # y = y.to(device)
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.08)
+    _, _ = ZINB_grad.train_ZINB(y, optimizer, model, epochs=150)
+
+    p = model(y)
+    assert not torch.any(torch.isclose(p, synth_data.p))
+
+# helper.plot_line(list(range(len(losses))), losses)
+#
+# w = model.W.cpu().detach().numpy()
+#
+# sil_coeff = helper.kmeans(w)
+#
+# helper.plot_line(list(range(len(sil_coeff))), sil_coeff)
+#
+#
+# labels = labels.numpy()
+#
+# helper.measure_q(w, labels, n_clusters=7)
